f7c1307ff42f27c00284edc0e9c6fb4fdd11fb58
Aditya sharma <131639817+giteadi@users.noreply.github.com>
2024-11-15 19:07:04 +0530

    Delete server directory

--- server/routes/formRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {submitContactForm} = require('../controllers/formController');
-
-// Route for submitting the contact form
-router.post('/submit', submitContactForm);
-
-module.exports = router;
